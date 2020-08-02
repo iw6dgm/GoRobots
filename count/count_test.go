8b5b23e41608356cfc73d6270f191257c4a999e7
joshua <iw6dgm@tiscali.it>
2020-08-02 17:04:03 +0100

    Fix count tests

--- count/count_test.go
+++ count/count_test.go
@@ -40,21 +40,21 @@ func TestParseLog(t *testing.T) {
 	r3 := result["1_1"]
 	r4 := result["null"]
 
-	if r1.games != 10 || r2.games != 10 || r3.games != 10 || r4.games != 10 {
-		t.Errorf("Invalid games count; want 10 10 10 10, got %d %d %d %d ", r1.games, r2.games, r3.games, r4.games)
+	if r1.Games != 10 || r2.Games != 10 || r3.Games != 10 || r4.Games != 10 {
+		t.Errorf("Invalid games count; want 10 10 10 10, got %d %d %d %d ", r1.Games, r2.Games, r3.Games, r4.Games)
 	}
 
-	if r1.wins != 3 || r2.wins != 5 || r3.wins != 1 || r4.wins != 0 {
-		t.Errorf("Invalid wins count; want 3 5 1 0, got %d %d %d %d ", r1.wins, r2.wins, r3.wins, r4.wins)
+	if r1.Wins != 3 || r2.Wins != 5 || r3.Wins != 1 || r4.Wins != 0 {
+		t.Errorf("Invalid wins count; want 3 5 1 0, got %d %d %d %d ", r1.Wins, r2.Wins, r3.Wins, r4.Wins)
 	}
 
-	if r1.ties[0] != 0 || r2.ties[0] != 1 || r3.ties[0] != 1 || r4.ties[0] != 0 {
-		t.Errorf("Invalid ties2 count; want 0 1 1 0, got %d %d %d %d ", r1.ties[0], r2.ties[0], r3.ties[0], r4.ties[0])
+	if r1.Ties[0] != 0 || r2.Ties[0] != 1 || r3.Ties[0] != 1 || r4.Ties[0] != 0 {
+		t.Errorf("Invalid ties2 count; want 0 1 1 0, got %d %d %d %d ", r1.Ties[0], r2.Ties[0], r3.Ties[0], r4.Ties[0])
 	}
 
 	sum := 0
 	for i := 1; i < 3; i++ {
-		sum += r1.ties[i] + r2.ties[i] + r3.ties[i] + r4.ties[i]
+		sum += r1.Ties[i] + r2.Ties[i] + r3.Ties[i] + r4.Ties[i]
 	}
 
 	if sum > 0 {
