d4c9c561e1feceac05e5dc5d936b37412e19917c
M C <joshua@deepthought.it>
2020-09-05 22:18:24 +0100

    Worker pools - from golang by examples

--- gorobots.go
+++ gorobots.go
@@ -38,88 +38,6 @@ type match struct {
 	Robots []string
 }
 
-type process struct {
-	ID    int
-	Match match
-}
-
-type processor struct {
-	ID               int
-	ProcessorChannel chan chan process // used to communicate between dispatcher and workers
-	Channel          chan process
-	End              chan bool
-}
-
-// start processor
-func (p *processor) Start(crobotsExecutable string, opt string, tot int, result *sync.Map) {
-	go func() {
-		for {
-			p.ProcessorChannel <- p.Channel // when the processor is available place channel in queue
-			select {
-			case process := <-p.Channel: // processor has received job
-				process.Match.processCrobotsMatch(crobotsExecutable, opt, tot, result) // do work
-			case <-p.End:
-				return
-			}
-		}
-	}()
-}
-
-// end processor
-func (p *processor) Stop() {
-	log.Printf("worker [%d] is stopping\n", p.ID)
-	p.End <- true
-}
-
-// ProcessorChannel is a channel of available processors (a processor is a channel of processes)
-var ProcessorChannel = make(chan chan process)
-
-// Collector handles jobs to be sent to processors
-type Collector struct {
-	Process chan process // receives jobs to send to processors
-	End     chan bool    // when receives bool stops processors
-}
-
-// StartDispatcher starts processors and return their Collector
-func StartDispatcher(processorCount int, crobotsExecutable string, opt string, tot int, result *sync.Map) Collector {
-	var i int
-	var processors []processor
-	input := make(chan process) // channel to recieve process
-	end := make(chan bool)      // channel to spin down processors
-	collector := Collector{Process: input, End: end}
-
-	for i < processorCount {
-		i++
-		log.Println("Starting processor:", i)
-		processor := processor{
-			ID:               i,
-			Channel:          make(chan process),
-			ProcessorChannel: ProcessorChannel,
-			End:              make(chan bool),
-		}
-		processor.Start(crobotsExecutable, opt, tot, result)
-		processors = append(processors, processor) // stores processor
-	}
-
-	// start collector
-	go func() {
-		for {
-			select {
-			case <-end:
-				for _, p := range processors {
-					p.Stop() // stop processor
-				}
-				return
-			case process := <-input:
-				processor := <-ProcessorChannel // wait for available channel
-				processor <- process            // dispatch process to processor
-			}
-		}
-	}()
-
-	return collector
-}
-
 type tournamentConfig struct {
 	Label      string   `yaml:"label"`
 	MatchF2F   int      `yaml:"matchF2F"`
@@ -169,41 +87,36 @@ func fileExists(filename string) bool {
 	return !info.IsDir()
 }
 
-func generateCombinations(list []string, size int) <-chan match {
-	c := make(chan match)
+func generateCombinations(list []string, size int, c chan<- match) {
 	tot := len(list)
-	go func(c chan match) {
-		defer close(c)
-		switch size {
-		case 2:
-			for i := 0; i < tot-2; i++ {
-				for j := i + 1; j < tot-1; j++ {
-					c <- match{Robots: []string{list[i], list[j]}}
-				}
+	switch size {
+	case 2:
+		for i := 0; i < tot-2; i++ {
+			for j := i + 1; j < tot-1; j++ {
+				c <- match{Robots: []string{list[i], list[j]}}
 			}
-		case 3:
-			for i := 0; i < tot-3; i++ {
-				for j := i + 1; j < tot-2; j++ {
-					for k := j + 1; k < tot-1; k++ {
-						c <- match{Robots: []string{list[i], list[j], list[k]}}
-					}
-				}
-			}
-		case 4:
-			for i := 0; i < tot-4; i++ {
-				for j := i + 1; j < tot-3; j++ {
-					for k := j + 1; k < tot-2; k++ {
-						for z := k + 1; z < tot-1; z++ {
-							c <- match{Robots: []string{list[i], list[j], list[k], list[z]}}
-						}
-					}
-				}
-			}
-		default:
-			log.Fatal("Invalid size", size)
 		}
-	}(c)
-	return c
+	case 3:
+		for i := 0; i < tot-3; i++ {
+			for j := i + 1; j < tot-2; j++ {
+				for k := j + 1; k < tot-1; k++ {
+					c <- match{Robots: []string{list[i], list[j], list[k]}}
+				}
+			}
+		}
+	case 4:
+		for i := 0; i < tot-4; i++ {
+			for j := i + 1; j < tot-3; j++ {
+				for k := j + 1; k < tot-2; k++ {
+					for z := k + 1; z < tot-1; z++ {
+						c <- match{Robots: []string{list[i], list[j], list[k], list[z]}}
+					}
+				}
+			}
+		}
+	default:
+		log.Fatal("Invalid size", size)
+	}
 }
 
 func printRobots(tot int, result *sync.Map) {
@@ -270,6 +183,13 @@ func (m match) processCrobotsMatch(crobotsExecutable string, opt string, tot int
 		} else {
 			result.Store(name, count.Robot{Name: name, Games: robot.Games, Wins: robot.Wins, Ties: robot.Ties, Points: 0, Eff: 0.0})
 		}
+	}
+}
+
+func worker(id int, matches <-chan match, crobotsExecutable string, opt string, tot int, result *sync.Map, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for m := range matches {
+		m.processCrobotsMatch(crobotsExecutable, opt, tot, result)
 	}
 }
 
@@ -346,12 +266,15 @@ func main() {
 	log.Println("Start processing...")
 	start := time.Now()
 	var result sync.Map
-	var i int
-	collector := StartDispatcher(NumCPU, *crobotsExecutable, opt, tot, &result)
-	for r := range generateCombinations(robots, tot) {
-		i++
-		collector.Process <- process{ID: i, Match: r}
+	jobs := make(chan match, NumCPU)
+	var wg sync.WaitGroup
+	for w := 1; w <= NumCPU; w++ {
+		wg.Add(1)
+		go worker(w, jobs, *crobotsExecutable, opt, tot, &result, &wg)
 	}
+	generateCombinations(robots, tot, jobs)
+	close(jobs)
+	wg.Wait()
 	duration := time.Since(start)
 	log.Println("Completed in", duration)
 	printRobots(tot, &result)
