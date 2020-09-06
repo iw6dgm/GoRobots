3a340de0f4e8c0861a7743d0140ce56158c206ea
joshua <iw6dgm@tiscali.it>
2020-09-06 09:54:52 +0100

    Remove example config - add benchmark config

---
+++ bench.r
@@ -0,0 +1,84 @@
+
+int i,result;
+
+int fib(n) /* Funzione ricorsiva per il calcolo */
+{                            /* dei primi n termini della serie   */
+int f1, f2;                  /* di Fibonacci */
+
+  if (n == 0)
+    return (0);
+  else
+    if (n == 1)
+      return (1);
+    else
+    {
+      f1 = fib(n - 1);
+      f2 = fib(n - 2);
+      return (f1 + f2);
+    };
+}
+
+loop()
+{
+  if(!scan(result=(atan(fib(i)*1000000)),10)) cannon(result,700);
+  drive(result,1);
+  i+=1;
+}
+
+main()
+{
+
+  i:=0;
+  while (i<=1) {  loop();}
+  i:=0;
+  while (i<=2) {  loop();}
+  i:=0;
+  while (i<=3) {  loop();}
+  i:=0;
+  while (i<=4) {  loop();}
+  i:=0;
+  while (i<=5) {  loop();}
+  i:=0;
+  while (i<=6) {  loop();}
+  i:=0;
+  while (i<=7) {  loop();}
+  i:=0;
+  while (i<=8) {  loop();}
+  i:=0;
+  while (i<=9) {  loop();}
+  i:=0;
+  while (i<=10) {  loop();}
+  i:=0;
+  while (i<=11) {  loop();}
+  i:=0;
+  while (i<=12) {  loop();}
+  i:=0;
+  while (i<=13) {  loop();}
+  i:=0;
+  while (i<=14) {  loop();}
+  i:=0;
+  while (i<=15) {  loop();}
+  i:=0;
+  while (i<=16) {  loop();}
+  i:=0;
+  while (i<=17) {  loop();}
+  i:=0;
+  while (i<=18) {  loop();}
+  i:=0;
+  while (i<=19) {  loop();}
+  i:=0;
+  while (i<=20) {  loop();}
+  i:=0;
+  while (i<=21) {  loop();}
+  i:=0;
+  while (i<=22) {  loop();}
+  i:=0;
+  while (i<=23) {  loop();}
+  i:=0;
+  while (i<=24) {  loop();}
+  i:=0;
+  while (i<=25) {  loop();}
+
+}
+
+
