25f61e94186399908f489938a1abdfe3a8e70137
joshua <iw6dgm@tiscali.it>
2020-09-06 18:04:39 +0100

    Bump version to 1.11

--- gorobots.go
+++ gorobots.go
@@ -20,7 +20,7 @@ import (
 
 const (
 	// Version is the software version format v#.##-timestamp
-	Version = "v1.1-20200905"
+	Version = "v1.11-20200906"
 	// Separator is the OS dependent path separator
 	Separator = string(os.PathSeparator)
 	// RobotBinaryExt is the file extension of the compiled binary robot
